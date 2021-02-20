e6a43c16bdc16ae144e0614c5d626c3aee237fe3
Jeremy Parker <jeremyeparker@yahoo.com>
2021-02-19 23:38:49 -0500

    create BankAcct class/ clear Main class

---
+++ atsignJar/BankAccount.java
@@ -0,0 +1,17 @@
+package atsignJar;
+
+public class BankAccount {
+    private int balance;
+    private String owner;
+    private String accountNum;
+    private String type;
+
+    public BankAccount(int balance, String owner, String accountNum, String type){
+        this.balance = balance;
+        this.owner = owner;
+        this.accountNum = accountNum;
+        this.type = type;
+    }
+}
+
+
--- atsignJar/Main.java
+++ atsignJar/Main.java
@@ -3,13 +3,6 @@ package atsignJar;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Learning Java again!");
 
-        Car myCar = new Car("gold","Volvo","6849",2009);
-        System.out.println(myCar.model);
-        myCar.model = "850";
-        System.out.println(myCar.model);
-        System.out.println(myCar.toString());
-        myCar.displayDetails();
     }
 }
