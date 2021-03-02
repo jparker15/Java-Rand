e786801f59149901b309b8afa5f643966fe7b539
Jeremy Parker <jeremyeparker@yahoo.com>
2021-03-02 13:50:22 -0500

    add mySavings variable/ test methods

--- atsignJar/Main.java
+++ atsignJar/Main.java
@@ -1,22 +1,29 @@
 package atsignJar;
 
-import Account.BankAccount;
-import Account.CheckingAccount;
+import Account.*;
 
 public class Main {
 
     public static void main(String[] args) {
         BankAccount myAcct = new BankAccount(100000,"Jar", "6969","Savings");
         CheckingAccount myChecking = new CheckingAccount(100000,"Jar", "6969", 50000);
-        System.out.println(myAcct.toString());
-        System.out.println(myChecking.toString());
-        myAcct.deposit(1000000);
-        System.out.println(myAcct.toString());
-        myAcct.withdraw(1900);
-        myChecking.withdraw(20000);
-        System.out.println(myAcct.toString());
-        System.out.println(myChecking.toString());
-        myChecking.withdraw(50000);
-        System.out.println(myChecking.toString());
+        SavingsAccount mySavings = new SavingsAccount(300000,"Zar","2058",5);
+//        System.out.println(myAcct.toString());
+//        System.out.println(myChecking.toString());
+        System.out.println(mySavings);
+//        myAcct.deposit(1000000);
+//        System.out.println(myAcct.toString());
+        mySavings.deposit(100);
+        System.out.println(mySavings);
+//        myAcct.withdraw(1900);
+        mySavings.withdraw(50);
+        System.out.println(mySavings);
+        mySavings.applyInterest();;
+        System.out.println(mySavings);
+//        myChecking.withdraw(20000);
+//        System.out.println(myAcct.toString());
+//        System.out.println(myChecking.toString());
+//        myChecking.withdraw(50000);
+//        System.out.println(myChecking.toString());
     }
 }
