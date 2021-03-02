73fe1f85ace56bed97719ebfe3d26d57412a366c
Jeremy Parker <jeremyeparker@yahoo.com>
2021-03-02 13:48:30 -0500

    create unique fields, add constructor, add applyInvestment method

--- Account/InvestmentAccount.java
+++ Account/InvestmentAccount.java
@@ -1,2 +1,18 @@
-package Account;public class InvestmentAccount {
+package Account;
+
+public class InvestmentAccount extends BankAccount{
+    private int investment;
+    private int period = 3;
+
+    public InvestmentAccount(int balance, String owner, String accountNum, String type, int investment){
+        super(balance,owner,accountNum,"Investment");
+        this.investment = investment;
+
+    }
+
+    private void applyInvestment(){
+        int appliedInvestment = balance * (investment/100);
+
+    }
+
 }
